9b8279eb50c20dc384d752a3ce790f62b914bbe2
Syed <syedsaifliton.com>
2025-09-28 17:11:23 +0530

    Api response

--- src/utils/asynHandler.js
+++ src/utils/asynHandler.js
@@ -1,5 +1,5 @@
 const asynHandler = (requestHandler) => {
-    (req,res,next) => {
+   return  (req,res,next) => {
         promise.resolve(requestHandler(req,res,next)).
         catch((err) => next(err))
     }
